74a18fe7f89aa8fc32f9ea3295224c254c2e8584
hangseung <yhs7604kor@gmail.com>
2020-06-08 21:50:14 +0900

    Add sign-up function

--- src/main/java/com/app/lockstar/UserController.java
+++ src/main/java/com/app/lockstar/UserController.java
@@ -1,12 +1,27 @@
 package com.app.lockstar;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.google.common.hash.Hashing;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.charset.StandardCharsets;
 
 @RestController
+@RequestMapping(path="/user")
 public class UserController {
-    @GetMapping("/user")
-    public String user() {
-        return "Hello World!";
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(path="/add")
+    public @ResponseBody String addNewUser (@RequestParam String name, @RequestParam String password) {
+        User n = new User();
+        n.setName(name);
+        n.setPassword(Hashing.sha256()
+                .hashString(password, StandardCharsets.UTF_8)
+                .toString());
+
+        userRepository.save(n);
+        return "Saved";
     }
 }
