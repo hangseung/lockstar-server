adc87c67817c11c10a4c85f0b1d8eb243ca3b342
hangseung <yhs7604kor@gmail.com>
2020-06-09 22:00:21 +0900

    Alter username unique

--- src/main/java/com/app/lockstar/UserController.java
+++ src/main/java/com/app/lockstar/UserController.java
@@ -2,9 +2,12 @@ package com.app.lockstar;
 
 import com.google.common.hash.Hashing;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path="/user")
@@ -14,14 +17,21 @@ public class UserController {
     private UserRepository userRepository;
 
     @PostMapping(path="/add")
-    public @ResponseBody String addNewUser (@RequestParam String name, @RequestParam String password) {
-        User n = new User();
-        n.setName(name);
-        n.setPassword(Hashing.sha256()
+    @ResponseBody
+    public ResponseEntity addNewUser (@RequestParam String name, @RequestParam String password) {
+        Optional<User> sameNameUser = userRepository.findByName(name);
+        if (sameNameUser.isPresent()) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+
+        User newUser = new User();
+        newUser.setName(name);
+        newUser.setPassword(Hashing.sha256()
                 .hashString(password, StandardCharsets.UTF_8)
                 .toString());
 
-        userRepository.save(n);
-        return "Saved";
+        userRepository.save(newUser);
+
+        return new ResponseEntity(HttpStatus.ACCEPTED);
     }
 }
