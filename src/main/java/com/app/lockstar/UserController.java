0aeaf38c02671b1100e8d93f4808f38ca71d684d
hangseung <yhs7604kor@gmail.com>
2020-06-24 18:06:04 +0900

    Add an api for get server public key

--- src/main/java/com/app/lockstar/UserController.java
+++ src/main/java/com/app/lockstar/UserController.java
@@ -1,7 +1,10 @@
 package com.app.lockstar;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +21,9 @@ public class UserController {
 
     @Autowired
     private S3Service s3Service;
+
+    @Value("${server_public_key_location}")
+    private String serverPublicKeyLocation;
 
     @PostMapping(path="/add")
     @ResponseBody
@@ -54,5 +60,26 @@ public class UserController {
         userService.update(user);
 
         return new ResponseEntity(filePath, HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping(path="/server_public_key")
+    @ResponseBody
+    public ResponseEntity getServerPublicKey (@RequestParam("username") String username, @RequestParam("password") String password) {
+        try {
+            userService.signIn(username, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
+        try {
+            Resource serverPublicKeyResource = s3Service.download(serverPublicKeyLocation);
+
+            return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                    "attachment; filename=\"" + serverPublicKeyResource.getFilename() + "\"").body(serverPublicKeyResource);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
